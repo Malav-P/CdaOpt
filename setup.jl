7e1c979f49c20de1e8902eb600b42c3b9cd99ccc
Malav_P <malavpatel2022@u.northwestern.edu>
2024-05-21 23:07:19 -0400

    setup file

---
+++ setup.jl
@@ -0,0 +1,12 @@
+using Pkg
+
+ENV["GUROBI_HOME"] = "/Library/gurobi1000/macos_universal2/"
+
+# Specify the path to the project directory
+project_dir = @__DIR__
+
+# Activate the project environment
+Pkg.activate(project_dir)
+
+# Instantiate the environment, installing all dependencies as specified in Project.toml and Manifest.toml
+Pkg.instantiate()
